fbe9bbd1965411f8f06cbc4a91aee1b4c7305d33
KaiserFrost <brunossj5@hotmail.com>
2020-04-30 21:31:53 +0100

    aaaa

--- ftp-image/docker-entrypoint.sh
+++ ftp-image/docker-entrypoint.sh
@@ -5,6 +5,6 @@ apt -y install git
 apt -y install python3.7
 apt -y install python3-pip
 git clone https://github.com/KaiserFrost/TP2VR.git
-cd TP2VR/ftp-image/ftp/
+cd TP2VR/ftp-image/
 pip3 install -r requirements.txt
 python3 ftpserver.py
