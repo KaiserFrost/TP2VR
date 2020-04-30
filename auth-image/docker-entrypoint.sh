6ae9b898a45698111eaa34a92a21959151f07a0b
KaiserFrost <brunossj5@hotmail.com>
2020-04-30 22:17:02 +0100

    one more

--- auth-image/docker-entrypoint.sh
+++ auth-image/docker-entrypoint.sh
@@ -4,7 +4,6 @@ apt update
 apt -y install git
 apt -y install python3.7
 apt -y install python3-pip
-sudo apt-get install libpq-dev
 git clone https://github.com/KaiserFrost/TP2VR.git
 cd TP2VR/auth-image/
 #pip3 install libpq-dev
@@ -13,5 +12,6 @@ cd TP2VR/auth-image/
 #pip3 install Flask
 #pip3 install pyjwt
 #pip3 install python-dotenv
+sudo apt-get install libpq-dev
 pip3 install -r requirements.txt
 python3 testflask.py
