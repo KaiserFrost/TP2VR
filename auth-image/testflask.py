5ab23af468decc967d594cba40b9e53b60927b72
KaiserFrost <brunossj5@hotmail.com>
2020-04-30 22:53:13 +0100

    mmm

--- auth-image/testflask.py
+++ auth-image/testflask.py
@@ -87,4 +87,4 @@ def register():
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
-    app.run(host="localhost", port=5001,debug=True)
+    app.run(host="0.0.0.0", port=5001,debug=True)
